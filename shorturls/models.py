eb12df2f93088d75a55eb719b9c1427d596b3fce
Renato Oliveira <renato@labcodes.com.br>
2017-09-04 15:01:03 -0300

    Validate before saving shorturls.

--- shorturls/models.py
+++ shorturls/models.py
@@ -2,12 +2,13 @@ import random
 import string
 
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class ShortURL(models.Model):
 
     original = models.URLField(max_length=500)
-    shortened_slug = models.CharField(max_length=6, blank=True)
+    shortened_slug = models.CharField(max_length=6, blank=True, unique=True)
 
     clicks = models.IntegerField(default=0)
 
@@ -15,10 +16,15 @@ class ShortURL(models.Model):
         self.clicks += 1
         self.save()
 
+    def clean(self):
+        if self.shortened_slug == 'admin':
+            raise ValidationError('Slug "admin" cannot be created')
+
     def __str__(self):
         return f'http://lab.codes/{self.shortened_slug} -> {self.original}'
 
     def save(self, *args, **kwargs):
+        self.clean()
         if not self.pk:
             if not self.shortened_slug:
                 self.shortened_slug = ''.join(random.sample(string.ascii_lowercase + string.digits, 6))
